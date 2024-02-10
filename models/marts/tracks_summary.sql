53ce08ce7c93901eda192aca0b13800b73f80415
mtrentz <mateus.trentz@gmail.com>
2024-02-10 14:11:04 +0000

    test sync

--- models/marts/tracks_summary.sql
+++ models/marts/tracks_summary.sql
@@ -9,34 +9,7 @@ with
 
     streaming_history_fully_joined as (
 
-        select
-            sh.played_at,
-            sh.ms_played,
-            sh.minutes_played,
-            sh.hours_played,
-            sh.context,
-            sh.reason_start,
-            sh.reason_end,
-            sh.is_skipped,
-            sh.is_shuffled,
-            t.track_id,
-            t.track_name,
-            t.track_disc_number,
-            t.track_duration,
-            t.track_is_explicit,
-            t.track_popularity,
-            t.track_track_number,
-            t.track_is_local,
-            al.album_id,
-            al.album_name,
-            al.album_label,
-            al.album_popularity,
-            al.album_release_date,
-            al.album_total_tracks,
-            ar.artist_id,
-            ar.artist_name,
-            ar.artist_popularity,
-            ar.artist_followers
+        select sh.played_at, sh.ms_played, t.track_id, al.album_id
         from streaming_history sh
         left join tracks t on sh.track_id = t.track_id
         left join albums al on t.track_album_id = al.album_id
