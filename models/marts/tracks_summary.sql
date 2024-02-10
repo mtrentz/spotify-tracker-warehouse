3e0b0fe7951b1d46ecaddbfe8949c5008bcf86ab
mtrentz <mateus.trentz@gmail.com>
2024-02-10 15:53:02 +0000

    tracks summary mart

--- models/marts/tracks_summary.sql
+++ models/marts/tracks_summary.sql
@@ -7,20 +7,45 @@ with
 
     artists as (select * from {{ ref("stg__artists") }}),
 
-    streaming_history_fully_joined as (
-
+    track_stats as (
         select
-            sh.played_at,
-            t.track_duration,
-            t.track_popularity,
-            t.track_is_explicit,
-            t.track_is_local
+            sh.track_id,
+            count(sh.track_id) as times_played,
+            sum(sh.ms_played) as total_ms_played,
+            sum(sh.minutes_played) as total_minutes_played,
+            sum(sh.hours_played) as total_hours_played,
+            min(sh.played_at) as first_played_at,
+            max(sh.played_at) as last_played_at
         from streaming_history sh
-        left join tracks t on sh.track_id = t.track_id
-        left join albums al on t.track_album_id = al.album_id
-        left join artists ar on t.track_main_artist_id = ar.artist_id
-
+        group by sh.track_id
     )
 
-select *
-from streaming_history_fully_joined
+select
+    ts.track_id,
+    ts.times_played,
+    ts.total_ms_played,
+    ts.total_minutes_played,
+    ts.total_hours_played,
+    ts.first_played_at,
+    ts.last_played_at,
+    t.track_name,
+    t.track_disc_number,
+    t.track_duration,
+    t.track_is_explicit,
+    t.track_popularity,
+    t.track_track_number,
+    t.track_is_local,
+    al.album_id,
+    al.album_name,
+    al.album_label,
+    al.album_popularity,
+    al.album_release_date,
+    al.album_total_tracks,
+    ar.artist_id,
+    ar.artist_name,
+    ar.artist_popularity,
+    ar.artist_followers
+from track_stats ts
+left join tracks t on ts.track_id = t.track_id
+left join albums al on t.track_album_id = al.album_id
+left join artists ar on t.track_main_artist_id = ar.artist_id
