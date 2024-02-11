5bd53e1b8f39584168d1c4ffbde52262bcaad795
mtrentz <mateus.trentz@gmail.com>
2024-02-11 11:43:05 +0000

    new stuff!

--- models/intermediate/intermediate__artist_stats.sql
+++ models/intermediate/intermediate__artist_stats.sql
@@ -19,7 +19,11 @@ select
     sum(sh.minutes_played) as total_minutes_played,
     sum(sh.hours_played) as total_hours_played,
     min(sh.played_at) as first_played_at,
-    max(sh.played_at) as last_played_at
+    max(sh.played_at) as last_played_at,
+    coalesce(sum(is_skipped::int), 0) as times_skipped,
+    coalesce(sum(is_skipped::int) / count(sh.track_id), 0) as skip_rate,
+    coalesce(count(*) filter (where reason_start = 'clickrow'), 0) as manual_plays,
+    avg(sh.ms_played) / 60000 as avg_track_listen_minutes
 from streaming_history sh
 -- Here I will use the m2m table to join tracks and artsits
 -- which mean a track can count for minutes into multiple artists at once!
--- models/intermediate/intermediate__track_stats.sql
+++ models/intermediate/intermediate__track_stats.sql
@@ -9,6 +9,9 @@ select
     sum(sh.minutes_played) as total_minutes_played,
     sum(sh.hours_played) as total_hours_played,
     min(sh.played_at) as first_played_at,
-    max(sh.played_at) as last_played_at
+    max(sh.played_at) as last_played_at,
+    coalesce(sum(is_skipped::int), 0) as times_skipped,
+    coalesce(sum(is_skipped::int) / count(sh.track_id), 0) as skip_rate,
+    coalesce(count(*) filter (where reason_start = 'clickrow'), 0) as manual_plays
 from streaming_history sh
 group by sh.track_id
