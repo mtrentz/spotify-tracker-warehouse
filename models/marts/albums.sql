1721c3361e7aded8272f0332effbc31ca60cd2f3
mtrentz <mateus.trentz@gmail.com>
2024-02-18 11:16:18 +0000

    all album artists

--- models/marts/albums.sql
+++ models/marts/albums.sql
@@ -2,9 +2,12 @@ with
 
     artists as (select * from {{ ref("stg__artists") }}),
 
-    albums as (select * from {{ ref("stg__albums") }})
+    albums as (select * from {{ ref("stg__albums") }}),
 
-select al.*, ar.artist_name as album_main_artist_name
+    all_album_artists as (select * from {{ ref("intermediate__all_album_artists") }})
+
+select al.*, ar.artist_name as album_main_artist_name, aaa.artists as all_album_artists
 
 from albums al
 left join artists ar on al.album_main_artist_id = ar.artist_id
+left join all_album_artists aaa on al.album_id = aaa.album_id
