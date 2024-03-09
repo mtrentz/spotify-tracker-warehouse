c430efd9972dfd9e5ee9a7b95b4af6d0500915ce
mtrentz <mateus.trentz@gmail.com>
2024-03-09 10:51:20 +0000

    fix

--- macros/map_superset_time_grain_to_postgres.sql
+++ macros/map_superset_time_grain_to_postgres.sql
@@ -1,13 +1,12 @@
 {% macro map_superset_time_grain_to_postgres_unit(time_grain) %}
-    {% set time_grain_upper = time_grain.upper() %}
-    {% if time_grain_upper == "P1D" %} {% set postgres_unit = "day" %}
-    {% elif time_grain_upper == "P1W" %} {% set postgres_unit = "week" %}
-    {% elif time_grain_upper == "P1M" %} {% set postgres_unit = "month" %}
-    {% elif time_grain_upper == "P3M" %} {% set postgres_unit = "quarter" %}
-    {% elif time_grain_upper == "P1Y" %} {% set postgres_unit = "year" %}
-    {% elif time_grain_upper == "PT1S" %} {% set postgres_unit = "second" %}
-    {% elif time_grain_upper == "PT1M" %} {% set postgres_unit = "minute" %}
-    {% elif time_grain_upper == "PT1H" %} {% set postgres_unit = "hour" %}
+    {% if time_grain == "p1d" %} {% set postgres_unit = "day" %}
+    {% elif time_grain == "p1w" %} {% set postgres_unit = "week" %}
+    {% elif time_grain == "p1m" %} {% set postgres_unit = "month" %}
+    {% elif time_grain == "p3m" %} {% set postgres_unit = "quarter" %}
+    {% elif time_grain == "p1y" %} {% set postgres_unit = "year" %}
+    {% elif time_grain == "pt1s" %} {% set postgres_unit = "second" %}
+    {% elif time_grain == "pt1m" %} {% set postgres_unit = "minute" %}
+    {% elif time_grain == "pt1h" %} {% set postgres_unit = "hour" %}
     {% else %} {% set postgres_unit = time_grain %}
     {% endif %}
     {{ return(postgres_unit) }}
